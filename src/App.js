00f5ded852fa570cb86546cf1b559d53467bba0c
esakkidass <68777705+esakidoss@users.noreply.github.com>
2025-03-07 19:54:38 +0530

    second-attempt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Weather from './Weather';
 function App() {
   return (
-    <><h1>Weather App</h1>
+    <>
     <Weather />
     </>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -4,63 +4,56 @@ import humidity from'./humidity.png';
 import temp from './temprature.png';
 import wind from './icons/wind.png';
 import cloud from './icons/cloudy.png';
+import arrow from './icons/down-chevron.png';
 import axios from "axios";
 
 
 const Weather = () => {
-  const [city, setCity] = useState("");
   const [weathe, setWeathe] = useState(null);
   const mycity="Chennai";
 
   const API_KEY = "95c9d6d986cc62be2f390e8f2f8284da"; 
   const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${mycity}&appid=${API_KEY}&units=metric`;
 
-  const fetchWeather = async () => {
+    const result=async()=>{
     try {
-      const response = await axios.get(API_URL);
-      console.log(response);
+      const response =await axios.get(API_URL);
       setWeathe(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
       setWeathe(null);
     }
   };
-
   return (
-    <div className="wether">
-      <h2>Weather App</h2>
-      <input 
-        type="text" 
-        placeholder="Enter city name" 
-        value={city} 
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={fetchWeather}>Get Weather</button>
+    <div className="wether" >
+      <div className="reload">
+         <img src={arrow} alt="reload" onClick={result} />
+      </div>
       {weathe && (
-        <div className="container-holder"> 
+        <div className="full-container">
           <div className="container">
-          <h3>CITY: {weathe.name}</h3>
-          <h3>COUNTRY: {weathe.sys.country}</h3>
-          <h3>{weathe.sys.sunrise}</h3>
-        </div>
-        <div className="container">
-          <div className="container-items">
-          <img src={temp} alt="Temp" /><br />
-          <img src={humidity} alt="humidity" />
+             <h1>{weathe.name}</h1>
+             <p id="count">{weathe.sys.country}</p>
+             <img src={cloud} alt="clo" />
+             <p className="weath">{weathe.weather[0].description}</p>
           </div>
-          <div> 
-          <p>Temperature: {weathe.main.temp}°C</p>
-          <p>HUMIDITY :{weathe.main.humidity}</p>
+          <div className="container-2">
+            <div>
+              <h1 className="temp">{weathe.main.temp} <br />%c</h1>
+            </div>
+            <div className="wind-info">
+              <div><img src={wind} alt="wind" />
+              <h1 className="windy">{weathe.wind.speed} w</h1>
+              <h6>Wind</h6>
+              </div>
+              <div>
+              <img src={humidity} alt="humidity" />
+              <h1 className="humi">{weathe.main.humidity} H</h1>
+              <h6>Humidity</h6>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="container">
-          <div className="container-items"><img src={cloud} alt="cloud" />
-          <img src={wind} alt="WIND" />
-          </div>
-          <div><p>Weather: {weathe.weather[0].description}</p>
-          <p>Wind:{weathe.wind.speed}</p></div>
-        </div>
-
         </div>
       )}
     </div>
